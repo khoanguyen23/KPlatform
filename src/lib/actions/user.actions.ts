e7f1288ff851bc3863f1737e596a0d205500d6f1
khoanguyen23 <khoahoang151@gmail.com>
2024-07-11 11:33:52 +0700

    validate user go to add course page

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,13 +1,25 @@
 "use server";
 
-import User from "@/database/user.model";
+import User, { IUser } from "@/database/user.model";
 import { connect } from "../mongoose";
+import { TCreateUserParams } from "@/types";
 
-export async function createUser(user: any) {
+export async function createUser(user: TCreateUserParams) {
   try {
     await connect();
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getUserInfo({ userId }: { userId: string }): Promise<IUser | null | undefined> {
+  try {
+    await connect();
+    const findUser = await User.findOne({ clerkId: userId });
+    if (!findUser) return null;
+    return findUser;
   } catch (error) {
     console.log(error);
   }
