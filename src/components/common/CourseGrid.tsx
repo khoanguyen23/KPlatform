3356db027a7dd7d88151f602a1b5eba242b29e3f
khoanguyen23 <khoahoang151@gmail.com>
2024-07-24 00:48:16 +0700

    💄 style: responsive homepage

--- src/components/common/CourseGrid.tsx
+++ src/components/common/CourseGrid.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CourseGrid = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-6 gap-8 mt-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-8 mt-6">
       {children}
     </div>
   );
