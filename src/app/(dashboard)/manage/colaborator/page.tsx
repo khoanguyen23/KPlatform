b35bfce8baa3052285cef0de965f63bb9cd05c52
khoanguyen23 <khoahoang151@gmail.com>
2024-07-27 01:19:57 +0700

    ✨ feat: image converter ( copy from Hong Hong)

--- src/app/(dashboard)/manage/colaborator/page.tsx
+++ src/app/(dashboard)/manage/colaborator/page.tsx
@@ -5,7 +5,72 @@ import { useAuth } from "@clerk/nextjs";
 import { getUserInfo, requestExpert } from "@/lib/actions/user.actions";
 import { EUserRole } from "@/types/enums";
 import { IUser } from "@/database/user.model";
-import {Spinner} from "@nextui-org/spinner";
+import { Audio, Blocks } from "react-loader-spinner";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+
+const invoices = [
+  {
+    invoice: "NHDTA-821",
+    paymentStatus: "https://njavtv.com/vi/nhdta-821-uncensored-leak",
+    totalAmount: "$250.00",
+    paymentMethod: "Seino Ayaba",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
 const Page = () => {
   const [isExpertRequested, setIsExpertRequested] = useState(false);
@@ -13,7 +78,6 @@ const Page = () => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [userInfo, setUserInfo] = useState<IUser | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-
 
   const router = useRouter();
   const { userId } = useAuth();
@@ -51,19 +115,84 @@ const Page = () => {
   };
 
   if (loading) {
-    return  <Spinner color="primary"/>; // Hoặc bạn có thể sử dụng một spinner/loading indicator ở đây.
+    return (
+      <Blocks
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        visible={true}
+      />
+    ); // Hoặc bạn có thể sử dụng một spinner/loading indicator ở đây.
   }
 
   return (
     <div>
       {isAdmin ? (
-        <div>
-          <h1>xin chào {userInfo?.name}</h1>
+        <div className="space-y-5">
+          <h1 className="uppercase font-bold text-xl">
+            xin chào <span className="text-red-700">{userInfo?.name}</span>
+          </h1>
           {/* Nội dung dành riêng cho admin */}
           <h2>Admin Dashboard</h2>
-          <p>
-            Welcome to the admin dashboard. Here you can manage users and roles.
-          </p>
+          <div className="flex justify-between">
+            <p>
+              Welcome to the admin dashboard. Here you can manage users and
+              roles.
+            </p>
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a fruit" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Fruits</SelectLabel>
+                  <SelectItem value="apple">njav</SelectItem>
+                  <SelectItem value="banana">Banana</SelectItem>
+                  <SelectItem value="blueberry">Blueberry</SelectItem>
+                  <SelectItem value="grapes">Grapes</SelectItem>
+                  <SelectItem value="pineapple">Pineapple</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[120px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {invoices.map((invoice) => (
+                <TableRow key={invoice.invoice}>
+                  <TableCell className="font-medium">
+                    {invoice.invoice}
+                  </TableCell>
+                  <TableCell>
+                    <Link legacyBehavior href={`${invoice.paymentStatus}`} passHref>
+                      <a target="_blank">Link</a>
+                    </Link>
+                  </TableCell>
+                  <TableCell>{invoice.paymentMethod}</TableCell>
+                  <TableCell className="text-right">
+                    {invoice.totalAmount}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total</TableCell>
+                <TableCell className="text-right">$2,500.00</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
         </div>
       ) : (
         <div>
