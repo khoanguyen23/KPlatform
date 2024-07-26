7b24e31c2d4925e493f110ad78da64403ea4e938
khoanguyen23 <khoahoang151@gmail.com>
2024-07-26 23:55:45 +0700

    ✨ feat: admin content in colaborator page

--- src/app/(dashboard)/manage/colaborator/page.tsx
+++ src/app/(dashboard)/manage/colaborator/page.tsx
@@ -4,11 +4,16 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "@clerk/nextjs";
 import { getUserInfo, requestExpert } from "@/lib/actions/user.actions";
 import { EUserRole } from "@/types/enums";
-
+import { IUser } from "@/database/user.model";
+import {Spinner} from "@nextui-org/spinner";
 
 const Page = () => {
   const [isExpertRequested, setIsExpertRequested] = useState(false);
   const [isExpert, setIsExpert] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [userInfo, setUserInfo] = useState<IUser | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
 
   const router = useRouter();
   const { userId } = useAuth();
@@ -16,9 +21,15 @@ const Page = () => {
   useEffect(() => {
     if (userId) {
       const fetchUserInfo = async () => {
+        setLoading(true);
         const user = await getUserInfo({ userId });
-        setIsExpertRequested(user?.expertRequest || false);
-        setIsExpert(user?.role === EUserRole.EXPERT);
+        if (user) {
+          setIsExpertRequested(user.expertRequest || false);
+          setIsExpert(user.role === EUserRole.EXPERT);
+          setIsAdmin(user.role === EUserRole.ADMIN);
+          setUserInfo(user);
+        }
+        setLoading(false);
       };
       fetchUserInfo();
     }
@@ -39,19 +50,35 @@ const Page = () => {
     }
   };
 
+  if (loading) {
+    return  <Spinner color="primary"/>; // Hoặc bạn có thể sử dụng một spinner/loading indicator ở đây.
+  }
+
   return (
     <div>
-    <h1>Account Page</h1>
-    {isExpert ? (
-      <p>You are an expert.</p>
-    ) : isExpertRequested ? (
-      <p>Your request to become an expert is pending approval.</p>
-    ) : (
-      <button onClick={handleExpertRequest}>
-        Request to become an Expert
-      </button>
-    )}
-  </div>
+      {isAdmin ? (
+        <div>
+          <h1>xin chào {userInfo?.name}</h1>
+          {/* Nội dung dành riêng cho admin */}
+          <h2>Admin Dashboard</h2>
+          <p>
+            Welcome to the admin dashboard. Here you can manage users and roles.
+          </p>
+        </div>
+      ) : (
+        <div>
+          {isExpert ? (
+            <p>You are an expert.</p>
+          ) : isExpertRequested ? (
+            <p>Your request to become an expert is pending approval.</p>
+          ) : (
+            <button onClick={handleExpertRequest}>
+              Request to become an Expert
+            </button>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
