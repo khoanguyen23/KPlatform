6ce97688852c7531b0bc775d8e846700c33396b5
khoanguyen23 <khoahoang151@gmail.com>
2024-08-03 03:24:08 +0700

    🐛 fix: react/no-unescaped-entities

--- src/app/(me)/page.tsx
+++ src/app/(me)/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <main className="section">
      <div className="container">
-        <h1 className="font-bold text-7xl bg-gradient-to-br from-orange-300  via-red-500 to-rose-300 bg-clip-text text-transparent">Hey, I'm Khoa</h1>
+        <h1 className="font-bold text-7xl bg-gradient-to-br from-orange-300  via-red-500 to-rose-300 bg-clip-text text-transparent">Hey, I&apos;m Khoa</h1>
         <p className="text-white italic text-lg mt-5 ml-5">"This is my portfolio"</p>
         <h2 className="bg-gradient-to-r from-blue-300 to-blue-800 bg-clip-text text-transparent text-4xl font-bold mt-10">
           About me
